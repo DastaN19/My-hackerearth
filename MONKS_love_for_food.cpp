5b381028ab0cfb83a53f72678de9522c4452992a
DastaN19 <ankitarnav201@gmail.com>
2016-08-25 00:56:15 -0700

    Add files via upload

---
+++ MONKS_love_for_food.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+
+#include<stdio.h>
+
+#include<stack>
+
+using namespace std;
+
+stack<int> stck;
+
+int main()
+
+{
+	int t,cu,ch;
+	
+	cin>>t;
+
+
+	while(t--)
+	
+	{
+
+		
+		cin>>cu;
+		
+		if(cu==1)
+		
+		{
+			
+			if(stck.empty())
+			
+				cout<<"No Food\n";
+			
+			else
+			
+			{
+				
+				cout<<stck.top()<<"\n";
+				
+				stck.pop();
+			
+			}
+		
+		}
+		
+		else
+		
+		{
+			
+			cin>>ch;
+
+			
+			stck.push(ch);
+		
+		} 
+	
+	}
+	
+	return 0;
+
+}
